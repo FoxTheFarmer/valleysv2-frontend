149503ad8a21668277bfd0c7e2a1c39b5b37276d
snappycreeper <anhochanadel@gmail.com>
2021-09-12 13:25:49 -0500

    first commit
    first commit

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,41 @@
+import labo from "./labo"
+
+export default {
+  cake: {
+    1666600000: labo.addr.laboAddr,
+    97: '',
+  },
+  masterChef: {
+    1666600000: labo.addr.masterChef, // cambiar
+    97: '',
+  },
+  // this is WONE "0xcf664087a5bb0237a0bad6742852ec6c8d69a27a"
+  wbnb: {
+    1666600000: '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a',
+    97: '',
+  },
+  lottery: {
+    1666600000: '0x7cA4ab6E901AaE4B3F164c7Fe1061d7B945fDf33',
+    97: '',
+  },
+  lotteryNFT: {
+    1666600000: '0xcf58a0751AA87AF64072a042C25137C8f65Fe674',
+    97: '',
+  },
+  mulltiCall: {
+    1666600000: '0xFE4980f62D708c2A84D3929859Ea226340759320',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    1666600000: '0xe176ebe47d621b984a73036b9da5d834411ef734',
+    97: '',
+  },
+  gya: {
+    1666600000: '0x9458784486fb00c2d1a8740d5579207c42dbd602',
+    97: '',
+  },
+  dsl: {
+    1666600000: '0x72feac4c0887c12db21ceb161533fd8467469e6b',
+    97: '',
+  },
+}
---
+++ src/config/constants/labo.ts
@@ -0,0 +1,133 @@
+// CONFIGURE HERE THE VARIABLES THAT WILL BE CHANGED BEFORE THE PRESALE
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// LABO ADDRESSES
+
+const laboBusdAddr = '0x306223ae4cd16a7623e9ff93bde461a60c5dcaf8'
+const laboBnbAddr = '0xf36067d88a49682d89638a3ed75431181585dbf4'
+const laboBusdAddrV2 = '0x306223ae4cd16a7623e9ff93bde461a60c5dcaf8'
+const laboBnbAddrV2 = '0xf36067d88a49682d89638a3ed75431181585dbf4'
+const laboAddr = '0xc078b878ae6BB14a8734d8a788F496984Cc7dde2'
+const masterChef = '0x21262941bd12FB01218afCF64AD0E23655E9F5f9'
+
+// LABO RELATED PIDS
+
+const pidLaboBusd = 0
+const pidLaboBnb = 1
+const pidLabo = 9
+
+// OTHER PIDS THAT ARE REFERENCED THROUGHOUT THE CODE
+
+const pidBnbBusd = 0
+
+// SET countBack TO TRUE TO COUNT BACK AS FAR AS countLength
+
+const countBack = false
+const countLength = 2
+
+// CHANGE LABO FOR THE TOKEN BEIGN TESTED
+
+const xPerBlock = "laboPerBlock" // referenced in getMasterChefAddress()
+const pendingX = "pendingLabo"
+
+// SEARCH AND REPLACE THIS STRING IF USING ANY OTHER THAN LABO: await masterChefContract.methods.pendingLabo in \src\state\pools\fetchPoolsUser.ts
+
+// USE OUR CUSTOM WAY OF FETCHING PRICE
+
+const fetchAutomatic = false;
+const fetchPriceCustom = false;
+
+// BitQuery RESPONSE POSITIONS
+
+const busdPosition = 0
+const tokenPosition = 1
+
+// IS LOCKED
+
+const unlockWalletButton = false;
+
+// IS FARMS CONFIGURED
+
+const isFullyConfigured = true;
+const showFarmInfoModal = true;
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// EDIT ABOVE NOT HERE
+
+const labo = {
+    addr: {
+        laboBusdAddr,
+        laboBnbAddr,
+        laboBusdAddrV2,
+        laboBnbAddrV2,
+        laboAddr,
+        masterChef,
+    },
+    pids: {
+        pidLaboBusd, 
+        pidLaboBnb,
+        pidLabo,
+        pidBnbBusd,
+        pidList: [
+            pidLaboBusd, // LABO-BUSD LP [0]
+            pidLaboBnb, // LABO-BNB LP [1]
+            pidBnbBusd, // BNB-BUSD LP [2]
+            /* 1, // USDT-BUSD LP [3]
+            2, // BTCB-BNB LP [4]
+            3, // ETH-BNB LP [5]
+            4, // DAI-BUSD LP [6]
+            5, // USDC-BUSD LP [7]
+            6, // DOT-BNB LP [8]
+            7, // CAKE-BUSD LP [9]
+            8, // CAKE-BNB LP [10]
+            pidLabo, // LABO [11]
+            10, // BUSD [12]
+            11, // WBNB [13]
+            12, // EGG [14]
+            18, // BTCB [15]
+            14, // ETH [16]
+            15, // DAI [17]
+            16, // USDC [18]
+            17, // DOT [19]
+            19, // BSCX [20]
+            13, // AUTO [21]
+            22, // LABO-GYA [22]
+            23, // LABO-DSL [23]
+            24, // LABO-BUSD LP V2 [24]
+            25, // LABO-BNB LP V2 [25]
+            26, // BUSD-BNB LP V2 [26]
+            27, // USDT-BUSD LP V2 [27]
+            28, // BTCB-BNB LP V2 [28] 
+            29, // ETH-BNB LP V2 [29]
+            30, // DAI-BUSD LP V2 [30] 
+            31, // USDC-BUSD LP V2 [31]
+            31, // DOT-BNB LP V2 [32]
+            32, // CAKE-BNB LP V2 [33]
+            33, // ADA-WBNB LP V2 [34] */
+
+
+        ],
+    },
+    strings: {
+        xPerBlock,
+        pendingX
+    },
+    fetch: {
+        fetchAutomatic,
+        fetchPriceCustom
+    },
+    queryPosition: {
+        busd: busdPosition,
+        token: tokenPosition
+    },
+    isLocked: {
+        unlockWalletButton
+    },
+    isFullyConfigured,
+    showFarmInfoModal
+}
+ 
+export default labo;
