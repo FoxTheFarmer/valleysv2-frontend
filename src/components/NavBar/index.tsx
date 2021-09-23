7cbfe1dbee0cf265a0274b015e1783b9721d668d
Ignacio Freijomil <babyjewelone@protonmail.com>
2021-09-23 20:26:09 -0300

    test

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -8,7 +8,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import UnlockButton from 'components/UnlockButton'
 import {Accordion, Button, Card, useAccordionToggle} from 'react-bootstrap';
-import { FaChartLine, FaTelegramPlane, FaTwitter, FaDiscord, FaFileAlt, FaGithub, FaTicketAlt, FaChartBar, FaMoneyBillAlt, FaTractor, FaHome, FaPrescriptionBottleAlt, FaTumblrSquare, FaCode, FaFlask, FaBook, FaReddit, FaRocketchat, FaRocket } from 'react-icons/fa';
+import { FaChartLine, FaTelegramPlane, FaTwitter, FaDiscord, FaFileAlt, FaGithub, FaTicketAlt, FaChartBar, FaMoneyBillAlt, FaTractor, FaHome, FaPrescriptionBottleAlt, FaTumblrSquare, FaCode, FaFlask, FaBook, FaReddit, FaRocketchat, FaRocket, FaBroadcastTower } from 'react-icons/fa';
 import ReactTooltip from 'react-tooltip';
 import labo from 'config/constants/labo';
 
@@ -231,6 +231,11 @@ const NavBar = (props) => {
                                   <span className="dditem">WHEEL</span>
                                 </Link>
                               </li>
+                              <li>
+                                <Link to="/" className="nav-links" onClick={()=>{setIsChecked(!isChecked)}}>
+                                  <span className="dditem">CRATES</span>
+                                </Link>
+                              </li>
                             </ul>
                           </li>
 
@@ -248,13 +253,23 @@ const NavBar = (props) => {
                               ABOUT
                             </Link>
                             <ul className="dropdown-content dropdown-items">
-                              <li>
-                                <a target="_blanK" rel="noreferrer" href="https://vfat.tools/harmony/" className="nav-links">
-                                  <FaChartLine /> <span className="dditem">VFAT</span>
+                            <li>
+                                <a target="_blanK" rel="noreferrer" href="https://bridge.harmony.one/" className="nav-links">
+                                <FaBroadcastTower /> <span className="dditem">BRIDGE</span>
+                                </a>
+                              </li>
+                            <li>
+                                <a target="_blanK" rel="noreferrer" href="https://artemis-protocol.gitbook.io/artemis-protocol/" className="nav-links">
+                                <FaBook /> <span className="dditem">DOCS</span>
                                 </a>
                               </li>
                               <li>
-                                <a target="_blanK" rel="noreferrer" href="https://twitter.com/ProtocolArtemis" className="nav-links">
+                                <a target="_blanK" rel="noreferrer" href="https://github.com/ArtemisProtocol" className="nav-links">
+                                  <FaCode /> <span className="dditem">CODE</span>
+                                </a>
+                              </li>
+                              <li>
+                                <a target="_blanK" rel="noreferrer" href="https://twitter.com/ArtemisProtoco1" className="nav-links">
                                 <FaTwitter />  <span className="dditem">TWITTER</span>
                                 </a>
                               </li>
@@ -273,16 +288,7 @@ const NavBar = (props) => {
                                 <FaDiscord />  <span className="dditem">DISCORD</span>
                                 </a>
                               </li>
-                              <li>
-                                <a target="_blanK" rel="noreferrer" href="https://artemis-protocol.gitbook.io/artemis-protocol/" className="nav-links">
-                                <FaBook /> <span className="dditem">DOCS</span>
-                                </a>
-                              </li>
-                              <li>
-                                <a target="_blanK" rel="noreferrer" href="https://github.com/ArtemisProtocol" className="nav-links">
-                                  <FaCode /> <span className="dditem">CODE</span>
-                                </a>
-                              </li>
+
                             </ul>
                           </li>
                           <InfoToggle />
